2a97263e1533692726bde4f0d802b84f1eabc4df
essserrr <blizzz93@gmail.com>
2021-08-27 12:06:23 +0900

    Video player is react-player based now

--- components/VideoPlayer/VideoPlayer.styled.tsx
+++ components/VideoPlayer/VideoPlayer.styled.tsx
@@ -1,14 +1,8 @@
 import styled from 'styled-components';
 
-const Frame = styled.iframe`
-  width: 100%;
-  height: 100%;
-  border: none;
-`;
-
 const PlayerWrapper = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 98vh;
 `;
 
-export { Frame, PlayerWrapper };
+export default PlayerWrapper;
--- components/VideoPlayer/VideoPlayer.tsx
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-
-import * as Styled from './VideoPlayer.styled';
+import ReactPlayer from 'react-player/lazy';
+import PlayerWrapper from './VideoPlayer.styled';
 
 interface IVideoPlayerProps {
   src: string;
+  play?: boolean;
 }
 
-const VideoPlayer: React.FC<IVideoPlayerProps> = ({ src }) => (
-  <Styled.PlayerWrapper>
-    <Styled.Frame allow="autoplay; fullscreen" src={`${src}?autoplay=1`} />
-  </Styled.PlayerWrapper>
+const VideoPlayer: React.FC<IVideoPlayerProps> = ({ src, play = false }) => (
+  <PlayerWrapper>
+    <ReactPlayer url={src} playing={play} controls width="100%" height="100%" />
+  </PlayerWrapper>
 );
 
+export type { IVideoPlayerProps };
 export default VideoPlayer;
