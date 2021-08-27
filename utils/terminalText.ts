5b6d1708372b9b0aa90825f6fa7298a640869c9a
essserrr <blizzz93@gmail.com>
2021-08-27 10:02:48 +0900

    messages -> terminal  text. text structure changed

---
+++ utils/terminalText.ts
@@ -0,0 +1,38 @@
+const messages = {
+  banner: `
+  Hello
+  World!
+  
+  help - prints available commands
+  `,
+  helpText: `
+  Available commands:
+  
+  help - This output
+  join - join gearbox 
+  clear - clear terminal
+  `,
+  metamaskConnected: `
+  Metamask connected
+  `,
+};
+
+const errors = {
+  commandNotFound: (c: string) => `
+  Command ${c} not found
+`,
+  noMetamask: `
+ Failed. Please install Metamask
+`,
+  metamaskNotConnected: `
+Failed to connect Metamask
+`,
+  metamaskWrongNetwork: `
+Please switch to Mainnet and try again.
+`,
+  metamaskLogin: `
+Log in and try again
+`,
+};
+
+export { messages, errors };
