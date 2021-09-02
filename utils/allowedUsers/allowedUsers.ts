e86b7ac58855dca10c463266c4479d09ca9d0c01
essserrr <blizzz93@gmail.com>
2021-09-02 09:59:10 +0900

    Allowed users typed

--- utils/allowedUsers/allowedUsers.ts
+++ utils/allowedUsers/allowedUsers.ts
@@ -1,5 +1,13 @@
-const usersList = {
-  '0xf13df765f3047850cede5aa9fdf20a12a75f7f70': true,
+interface IAccount {
+  index: number;
+  merklePath: Array<string>;
+}
+
+type AccountsList = Record<string, IAccount>;
+
+const usersList: AccountsList = {
+  '0xf13df765f3047850cede5aa9fdf20a12a75f7f70': { index: 1234678910, merklePath: ['1', '2', '3'] },
 };
 
-export default usersList;
+export type { IAccount, AccountsList };
+export { usersList };
