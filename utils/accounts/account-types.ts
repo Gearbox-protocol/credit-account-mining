5acf1a955655ee0924354dc3ad82ff80362b50bd
essserrr <blizzz93@gmail.com>
2021-09-08 12:37:16 +0900

    allowed users separated into types and accounts

--- utils/accounts/account-types.ts
+++ utils/accounts/account-types.ts
@@ -7,12 +7,4 @@ interface IAccount {
 
 type AccountsList = Record<string, IAccount>;
 
-const usersList: AccountsList = {
-  '0xf13df765f3047850cede5aa9fdf20a12a75f7f70': {
-    index: BigInt(9007199254740991),
-    merklePath: ['0x12', '0x23', '0x34'],
-  },
-};
-
 export type { IAccount, AccountsList };
-export { usersList };
