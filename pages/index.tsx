90750b77d2e200060994ace2d3a508a41f330135
essserrr <blizzz93@gmail.com>
2021-09-08 12:42:11 +0900

    Static server side page building added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
+// import fs from 'fs'; !!!!!!!!!!!
+import { GetStaticProps } from 'next';
 import { useSelector } from 'react-redux';
+import accounts from 'utils/accounts/accounts';
+import { constructAccountList } from 'utils/helpers/helpers';
 import { IState } from 'redux/root/rootReducer';
 import Layout from 'components/Layout/Layout';
 import FadingSwitcher from 'components/FadingSwitcher/FadingSwitcher';
@@ -11,7 +15,11 @@ const TerminalController = dynamic(
   { ssr: false },
 );
 
-const Index: React.FC = () => {
+interface IIndexProps {
+  error?: string;
+}
+
+const Index: React.FC<IIndexProps> = ({ error }) => {
   const { playVideo } = useSelector((state: IState) => state.terminalApp);
 
   return (
@@ -29,6 +37,28 @@ const Index: React.FC = () => {
       </FadingSwitcher>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const [userListRawRoute, userListTargetRoute] = [
+      process.env.USER_LIST_RAW_ROUTE,
+      process.env.USER_LIST_TARGET_ROUTE,
+    ];
+    if (!userListRawRoute || !userListTargetRoute) throw new Error('Routes not specified');
+
+    const [userList, root] = constructAccountList(accounts);
+
+    // const someData = [{ name: 'adman', tag: 'batsman', age: 25 }]; !!!!!!!!!!!
+    // const jsonData = JSON.stringify(someData); !!!!!!!!!!!
+    // const codeStr = `const cricketers = ${jsonData}; export { cricketers };`; !!!!!!!!!!!
+    // fs.writeFileSync('someFile.ts', codeStr, 'utf8'); !!!!!!!!!!!
+
+    return { props: { error: null } };
+  } catch (e: any) {
+    const props: IIndexProps = { error: e.message };
+    return { props };
+  }
 };
 
 export default Index;
