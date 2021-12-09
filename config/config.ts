b02bdef171e0a55f8479c2601b8e47f1c5baf87b
essserrr <blizzz93@gmail.com>
2021-12-09 18:24:11 +0900

    root and contract added

--- config/config.ts
+++ config/config.ts
@@ -1,8 +1,8 @@
 import { MerkleDistributorInfo } from 'utils/API/join/join';
 
 const distributorInfo: MerkleDistributorInfo = {
-  merkleRoot: '0xa654f6f122e357047995697bdf07d6b795df8c10749387d599b86460e104b878',
-  contract: '0x1417DD8865E30FC6619279Ce4eCeC373b7f5eFE6',
+  merkleRoot: process.env.NEXT_PUBLIC_MERKLE_ROOT || '',
+  contract: process.env.NEXT_PUBLIC_CONTRACT || '',
 };
 
 const network = process.env.NEXT_PUBLIC_GEARBOX_NETWORK || '42';
--- utils/accounts/distributor-info.ts
+++
@@ -1,8 +0,0 @@
-import { MerkleDistributorInfo } from 'utils/API/join/join';
-
-const distributorInfo: MerkleDistributorInfo = {
-  merkleRoot: '0xa654f6f122e357047995697bdf07d6b795df8c10749387d599b86460e104b878',
-  contract: '0x1417DD8865E30FC6619279Ce4eCeC373b7f5eFE6',
-};
-
-export default distributorInfo;
