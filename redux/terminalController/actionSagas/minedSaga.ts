770b3de886eb1ba8f73edfb857586c137a26fdd0
essserrr <blizzz93@gmail.com>
2021-12-14 17:46:17 +0900

    mined command doesn't require connection anumore

--- redux/terminalController/actionSagas/minedSaga.ts
+++ redux/terminalController/actionSagas/minedSaga.ts
@@ -3,11 +3,9 @@ import {
 } from 'redux-saga/effects';
 import messages from 'utils/API/messages/messages';
 import { getTypedError, TerminalError } from 'utils/API/errors/error-hub';
-import connectMetamask from 'utils/API/connect/connect';
 import { IClaimObject } from 'utils/API/join/join';
 import countClaims from 'utils/API/mined/mined';
 import { print, inputLock, loading } from 'redux/terminal/terminalAction';
-import { subscribe } from 'redux/subscription/subscriptionActions';
 import { cancelOnStatusChange } from 'redux/subscription/subscriptionSaga';
 import { setClaimObject } from 'redux/web3/web3Action';
 import { IState } from 'redux/root/rootReducer';
@@ -20,16 +18,12 @@ function* controllerMinedWorker() {
       web3: { claimObject },
     } = (yield select()) as IState;
     yield put(inputLock(true));
-
     yield put(loading(true));
-    yield call(connectMetamask);
-    yield put(subscribe());
 
     const [safeClaim, amount]: [IClaimObject, number] = yield call(countClaims, claimObject || {});
-
     if (!claimObject) yield put(setClaimObject(safeClaim));
-    yield put(loading(false));
 
+    yield put(loading(false));
     yield put(print({ msg: messages.accountsMined(amount) }));
     yield put(inputLock(false));
   } catch (e: any) {
--- utils/API/mined/mined.ts
+++ utils/API/mined/mined.ts
@@ -1,13 +1,21 @@
 import { AccountMining__factory } from '@diesellabs/gearbox-sdk/lib/types';
 import { AccountMining } from '@diesellabs/gearbox-sdk/src/types/AccountMining';
 import { ethers } from 'ethers';
+import { TerminalError } from 'utils/API/errors/TerminalError/TerminalError';
 import { IClaimObject } from 'utils/API/join/join';
-import { distributorInfo } from 'config/config';
+import { distributorInfo, network } from 'config/config';
 
 const countClaims = async (claimObj: Partial<IClaimObject>) => {
+  if (!window.ethereum || !window.ethereum!.isMetaMask) {
+    throw new TerminalError({ code: 'NO_METAMASK' });
+  }
+  if (window.ethereum.networkVersion !== network) {
+    throw new TerminalError({ code: 'METAMASK_WRONG_NETWORK' });
+  }
+
   const { contract } = distributorInfo;
   const {
-    provider = new ethers.providers.Web3Provider(window.ethereum!),
+    provider = new ethers.providers.Web3Provider(window.ethereum),
     signer = provider.getSigner(),
     miningAccount = <AccountMining>AccountMining__factory.connect(contract, signer),
   } = claimObj;
