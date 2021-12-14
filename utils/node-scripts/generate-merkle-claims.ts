c59009be8de7e97e3bb79ba732b7abc797e3ac32
essserrr <blizzz93@gmail.com>
2021-12-14 11:18:17 +0900

    merkle command added

--- utils/node-scripts/generate-merkle-claims.ts
+++ utils/node-scripts/generate-merkle-claims.ts
@@ -3,7 +3,6 @@ import path from 'path';
 import {
   MerkleDistributorInfo, ClaimsInfo, keyFromAddress, filename,
 } from 'utils/API/join/join';
-import jsonRaw from './merkle.json';
 
 type MerkleRaw = Omit<MerkleDistributorInfo, 'contract'> & { claims: ClaimsInfo };
 type MerkleSorted = Record<string, ClaimsInfo>;
@@ -29,15 +28,41 @@ const writeTo = (merkleRoute: string) => ([key, claims]: ClaimsPair) => {
   });
 };
 
+const readFrom = (merkleRoute: string) => {
+  const jsonString = fs.readFileSync(merkleRoute, 'utf8');
+  const merkleRaw: MerkleRaw = JSON.parse(jsonString);
+  return merkleRaw;
+};
+
+const ensureFolderExistence = (Folder: string) => {
+  fs.mkdir(Folder, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+};
+
+const clearFolder = (Folder: string) => {
+  const files = fs.readdirSync(Folder);
+  files.map((file) => fs.unlinkSync(path.join(Folder, file)));
+};
+
 function main() {
   const merkleRoute = process.env.CLAIMS_WRITE_ROUTE;
-  if (!merkleRoute) throw new Error('Merkle env not specified');
+  if (!merkleRoute) throw new Error('Merkle split destination env not specified');
 
-  const merkleRaw = jsonRaw as MerkleRaw;
+  const merkleSource = process.env.CLAIMS_MERKLE_SOURCE;
+  if (!merkleSource) throw new Error('merkle.json source env not specified');
+
+  ensureFolderExistence(merkleRoute);
+  clearFolder(merkleRoute);
+
+  const merkleRaw = readFrom(merkleSource);
   const merleSplit = split(merkleRaw);
   const writeClaims = writeTo(merkleRoute);
 
   Object.entries(merleSplit).forEach(writeClaims);
 }
+
+// start script
+if (process.env.CLAIMS_GENERATE === 'true') main();
 
 export default main;
