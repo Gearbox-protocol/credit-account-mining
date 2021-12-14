94d84ac93f9b2b5c2bc525467d7bd94a99bc06b4
essserrr <blizzz93@gmail.com>
2021-12-14 20:24:56 +0900

    claim limitations added

---
+++ components/ClaimLimitation/ClaimLimitation.tsx
@@ -0,0 +1,32 @@
+import { PropsWithChildren } from 'react';
+import { IState } from 'redux/root/rootReducer';
+import { useSelector } from 'react-redux';
+import Block from 'components/Block/Block';
+import Button from 'components/Button/Button';
+import { claimMaxCount } from 'config/config';
+import * as Styled from './ClaimLimitation.styled';
+
+interface IClaimLimitationProps {}
+
+function ClaimLimitation({ children }: PropsWithChildren<IClaimLimitationProps>) {
+  const { claimedCount } = useSelector((state: IState) => state.terminalApp);
+
+  const show = claimedCount < claimMaxCount;
+  return (
+    <>
+      {!show && (
+        <Block variant="viewport-sized">
+          <Styled.DisableWrap>
+            <Styled.TextWrap>
+              <Styled.Text>Error: already claimed, try your luck next time.</Styled.Text>
+            </Styled.TextWrap>
+            <Button title="Go back to website" href="https://app.beta.gearbox.fi/" />
+          </Styled.DisableWrap>
+        </Block>
+      )}
+      {show && children}
+    </>
+  );
+}
+
+export default ClaimLimitation;
--- config/config.ts
+++ config/config.ts
@@ -8,9 +8,10 @@ const distributorInfo: MerkleDistributorInfo = {
 const videoSource = process.env.NEXT_PUBLIC_VIDEO_SOURCE || '';
 const network = process.env.NEXT_PUBLIC_GEARBOX_NETWORK || '42';
 const claimsRoute = process.env.NEXT_PUBLIC_CLAIMS_ROUTE || '/';
+const claimMaxCount = Number(process.env.NEXT_PUBLIC_CLAIM_MAX_COUNT) || 0;
 const isDev = process.env.NODE_ENV === 'development';
 const isLive = process.env.NEXT_PUBLIC_APP_LIVE === 'true';
 
 export {
-  network, claimsRoute, distributorInfo, isDev, videoSource, isLive,
+  network, claimsRoute, distributorInfo, isDev, videoSource, isLive, claimMaxCount,
 };
