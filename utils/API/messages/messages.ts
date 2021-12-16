257af0326c9ec8fc2e435e050bcc5e1c727970f1
mikael <mikael.lazarev@gmail.com>
2021-12-16 01:55:22 +0100

    terms texts

--- utils/API/messages/messages.ts
+++ utils/API/messages/messages.ts
@@ -73,6 +73,15 @@ const messages = {
   claim: `
   Do you wish to mine a Credit Account? Type ${Prefix.PREFIX}${MineChoiceActions.YES} to proceed or ${Prefix.PREFIX}${MineChoiceActions.NO} to reject. 
 
+  By participating in Gearbox Credit Account mining, I agree to the Terms of Service (https://gearbox.fi/terms) and confirm that I have read and understood the Privacy Notice (https://gearbox.fi/privacy) and Risk Disclosure Statement (https://gearbox.fi/risks).
+  
+  I hereby further represent and warrant that:
+  
+  - I am not a citizen or resident of, or person subject to jurisdiction of, or located in, Cuba, Democratic People’s Republic of North Korea, Islamic Republic of Iran, Syria, the Crimea or Sevastopol, the People Republic of China (excluding Hong Kong, Macao and Taiwan), the United States of America (including its territories: American Samoa, Guam, Puerto Rico, the Northern Mariana Islands and the U.S. Virgin Islands), and shall not use or access Gearbox App while in any of the above territories.
+  - I am not subject to any sanctions administered or enforced by any country, government or international authority, and that I am not acting in the interests of such persons;
+  - I understand that if I fail to maintain sufficient collateral when using the Gearbox Protocol, my credit account(s) may be liquidated, in which case a penalty may be charged by the protocol.
+  - I acknowledge that Gearbox App and related software are experimental, and that the use of experimental software may result in complete loss of my funds.
+  
   Just to make sure once again… you understand what’s about to happen, right? 
 
   If no, then check this article for complete information: ${Links.CLAIM}
