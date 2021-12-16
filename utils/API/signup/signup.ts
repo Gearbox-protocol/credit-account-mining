7052e904f80c92f61f9cf5ea3d911a65c89fa7fc
mikael <mikael.lazarev@gmail.com>
2021-12-16 10:37:39 +0100

    short aggr

--- utils/API/signup/signup.ts
+++ utils/API/signup/signup.ts
@@ -23,13 +23,17 @@ function checkLogin(data: AgreementResponse) {
 
 const getFullUrl = (url: string) => `${backednAddress}${url}`;
 
-const agreement = 'By accessing or using Gearbox App, I agree to the Terms of Service (https://gearbox.fi/terms) and confirm that I have read and understood the Privacy Notice (https://gearbox.fi/privacy) and Risk Disclosure Statement (https://gearbox.fi/risks).\n'
-  + '\n'
-  + 'I hereby further represent and warrant that:\n'
-  + '- I am not a citizen or resident of, or person subject to jurisdiction of, or located in, Cuba, Democratic People’s Republic of North Korea, Islamic Republic of Iran, Syria, the Crimea or Sevastopol, the People Republic of China (excluding Hong Kong, Macao and Taiwan), the United States of America (including its territories: American Samoa, Guam, Puerto Rico, the Northern Mariana Islands and the U.S. Virgin Islands), and shall not use or access Gearbox App while in any of the above territories.\n'
-  + '- I am not subject to any sanctions administered or enforced by any country, government or international authority, and that I am not acting in the interests of such persons;\n'
-  + '- I understand that if I fail to maintain sufficient collateral when using the Gearbox Protocol, my credit account(s) may be liquidated, in which case a penalty may be charged by the protocol.\n'
-  + '- I acknowledge that Gearbox App and related software are experimental, and that the use of experimental software may result in complete loss of my funds.';
+const agreement = 'By accessing or using Gearbox App, I agree';
+
+
+// const agreement = 'By accessing or using Gearbox App, I agree to the Terms of Service (https://gearbox.fi/terms) and confirm that I have read and understood the Privacy Notice (https://gearbox.fi/privacy) and Risk Disclosure Statement (https://gearbox.fi/risks).\n'
+//     + '\n'
+//     + 'I hereby further represent and warrant that:\n'
+//     + '- I am not a citizen or resident of, or person subject to jurisdiction of, or located in, Cuba, Democratic People’s Republic of North Korea, Islamic Republic of Iran, Syria, the Crimea or Sevastopol, the People Republic of China (excluding Hong Kong, Macao and Taiwan), the United States of America (including its territories: American Samoa, Guam, Puerto Rico, the Northern Mariana Islands and the U.S. Virgin Islands), and shall not use or access Gearbox App while in any of the above territories.\n'
+//     + '- I am not subject to any sanctions administered or enforced by any country, government or international authority, and that I am not acting in the interests of such persons;\n'
+//     + '- I understand that if I fail to maintain sufficient collateral when using the Gearbox Protocol, my credit account(s) may be liquidated, in which case a penalty may be charged by the protocol.\n'
+//     + '- I acknowledge that Gearbox App and related software are experimental, and that the use of experimental software may result in complete loss of my funds.';
+
 
 const signup = async ({ miningAccount }: IClaimObject, address: string) => {
   const signature = await miningAccount.signer.signMessage(agreement);
