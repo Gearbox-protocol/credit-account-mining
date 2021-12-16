ad72ca2a895d28b3b291bff78e2e9e0ba31978e6
essserrr <blizzz93@gmail.com>
2021-12-16 10:18:03 +0900

    video hardcoded

--- config/config.ts
+++ config/config.ts
@@ -5,7 +5,7 @@ const distributorInfo: MerkleDistributorInfo = {
 };
 
 const claimMaxCount = Number(process.env.NEXT_PUBLIC_CLAIM_MAX_COUNT) || 0;
-const videoSource = process.env.NEXT_PUBLIC_VIDEO_SOURCE || '';
+const videoSource = 'https://youtu.be/2DZafHnECXs';
 const network = process.env.NEXT_PUBLIC_GEARBOX_NETWORK || '42';
 const claimsRoute = process.env.NEXT_PUBLIC_CLAIMS_ROUTE || '/';
 const isDev = process.env.NODE_ENV === 'development';
